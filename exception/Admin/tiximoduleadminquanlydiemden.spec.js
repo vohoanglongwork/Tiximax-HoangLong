a8cb89c4bc7ab79509117dec10e989ba8cd4fa95
vohoanglongwork <vohoanglong04112008@gmail.com>
2026-05-07 18:36:53 +0700

    UPDATE 07/05/2026

---
+++ exception/Admin/tiximoduleadminquanlydiemden.spec.js
@@ -0,0 +1,71 @@
+const { test, expect } = require('@playwright/test');
+const { time } = require('node:console');
+const { FORMERR } = require('node:dns');
+
+test('Test Accept Voucher', async ({ page }) => {
+  test.setTimeout(90000);
+
+
+
+  await page.goto('https://fe-new-staging.tiximax.net/auth/login');
+
+  await page.getByRole('button', { name: 'Tiếng Việt' }).click();
+  await page.getByRole('button', { name: 'Xác nhận' }).click();
+await page.getByRole('button', { name: 'Đăng nhập bằng mật khẩu' }).click();
+  await page.getByPlaceholder('Nhập tên đăng nhập').fill('autoManager');
+  await page.getByPlaceholder('••••••••').fill('123456');
+
+await Promise.all([
+  page.waitForURL('**/manager', { timeout: 15000 }),
+  page.getByRole('button', { name: 'Đăng nhập hệ thống' }).click()
+]);
+await expect(page).toHaveURL(/manager\/dashboard/);
+await page.waitForLoadState('networkidle');
+
+
+// đợi sidebar load
+await expect(
+  page.getByRole('link', { name: 'Xác nhận đơn hàng' })).toBeVisible({ force: true });
+
+  await page.getByRole('link', { name: 'Xác nhận đơn hàng' }).click({ force: true });
+await page.waitForLoadState('networkidle');
+await expect(page).toHaveURL(/manager\/quote/);
+await expect(page.getByText('Đã xác nhận').first()).toBeVisible();
+  
+     await page.getByRole('button', { name: 'Cấu hình hệ thống', exact: true }).click();
+
+  const employLink = page.getByRole('link', { name: 'Điểm đến' });
+  await expect(employLink).toBeVisible();
+  await employLink.click();
+
+
+  await expect(page).toHaveURL(/.*\/manager\/transfer/);
+
+  await expect(page.getByText('Quản Lý Điểm Đến').first()).toBeVisible();
+
+await page.getByRole('button', { name: 'Thêm điểm đến', exact: true }).first().click();
+await page.getByPlaceholder('VD: Hải Phòng, Đà Nẵng...').fill('Test Destination');
+
+await page.getByRole('button', { name: 'Lưu' }).click();
+
+
+   await expect(page.getByText('Tạo mới thành công!').first()).toBeVisible();
+const row = page.locator('tr', { hasText: 'Test Destination' }).first();
+const row2 = page.locator('tr', { hasText: 'Test Destination2' }).first();
+await row.scrollIntoViewIfNeeded();
+await expect(row).toBeVisible();
+await row.getByRole('button', { name: 'Sửa', exact: true }).click();
+
+
+
+
+await page.getByPlaceholder('VD: Hải Phòng, Đà Nẵng...').fill('Test Destination2');
+
+await page.getByRole('button', { name: 'Cập nhật', exact: true }).click();
+await expect(page.getByText('Cập nhật thành công!').first()).toBeVisible();
+await row2.scrollIntoViewIfNeeded();
+await expect(row2).toBeVisible();
+await row2.getByRole('button', { name: 'Xóa', exact: true }).first().click({ timeout: 15000 });
+await page.getByRole('button', { name: 'Xóa', exact: true }).last().click();
+  await expect(page.getByText('Xóa thành công!')).toBeVisible({ timeout: 15000 });
+});
---
+++ exception/Sale/Huỷ đơn/tiximodulesalehuydonchomua.spec.js
@@ -0,0 +1,60 @@
+const { test, expect } = require('@playwright/test');
+const { time } = require('node:console');
+const { TIMEOUT } = require('node:dns');
+const { link } = require('node:fs');
+
+test('Test full luong sale01', async ({ page }) => {
+  test.setTimeout(120000);
+
+  const randomUsername = `autosale_${Math.random().toString(36).substring(7)}`;
+const dkgName = `DKG${Date.now()}`;
+const randomMVD = `TESTMVD_${Math.random().toString(36).substring(7)}`; 
+
+
+  await page.goto('https://fe-new-staging.tiximax.net/auth/login');
+
+  await page.getByRole('button', { name: 'Tiếng Việt' }).click();
+  await page.getByRole('button', { name: 'Xác nhận' }).click();
+await page.getByRole('button', { name: 'Đăng nhập bằng mật khẩu' }).click();
+  await page.getByPlaceholder('Nhập tên đăng nhập').fill('autosale');
+  await page.getByPlaceholder('••••••••').fill('123456');
+
+  await page.getByRole('button', { name: 'Đăng nhập hệ thống' }).click({ timeout: 7000 });
+
+ 
+  await expect(page).toHaveURL(/create-orders\/purchase/);
+  await expect(page.getByText('Đang tải dữ liệu nền')).toBeHidden();
+await expect(page.getByText('Tạo đơn mua hộ')).toBeVisible();
+
+await page.getByRole('link', { name: 'Quản lý đơn hàng', exact: true }).click();
+const searchInput = page.getByPlaceholder('Tìm khách hàng');
+await searchInput.fill('Au');
+
+// scope vào vùng dropdown thay vì toàn page
+const dropdown = page.locator('div').filter({ has: searchInput });
+
+const customerOption = dropdown.getByRole('button', {
+  name: /AutoCustomer/
+});
+
+await expect(customerOption).toBeVisible();
+await customerOption.click();
+await page.getByRole('button', { name: 'Tìm kiếm' }).click();
+const daXacNhanBtn = page.getByRole('button', { name: /Chờ mua/ });
+
+await expect(daXacNhanBtn).toBeVisible();
+await daXacNhanBtn.click();
+await page.getByRole('button', { name: 'Xem' }).first().click({ timeout: 5000 });
+await expect(page.getByText('Đang tải chi tiết đơn hàng')).toBeHidden({ timeout: 10000 });
+const cancelBtn = page.getByRole('button', { name: 'Yêu cầu hủy đơn' });
+await expect(cancelBtn).toBeEnabled();
+await cancelBtn.click();
+await page.getByPlaceholder('Nhập lý do từ phía khách hàng...').fill('Test');
+await page.getByPlaceholder('Nhập ghi chú...').fill('Test 2');
+
+await page.getByRole('button', { name: 'Gửi yêu cầu' }).click();
+
+
+const successMsg = page.getByText('Đã gửi yêu cầu hủy đơn thành công.');
+  await expect(successMsg).toBeVisible();
+});
